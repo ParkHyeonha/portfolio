d7c315273b6991069d8c40cb3c839e7388b6d268
ParkHyeonha <a56054157@gmail.com>
2025-12-15 18:45:40 +0900

    헤더 링크를 수정하고 스크롤 시 헤더 스타일 개선

--- script/script.js
+++ script/script.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const grid = document.querySelector(".grid");
-  if (!grid) return;
+// header scroll effect
+window.addEventListener("scroll", () => {
+	const header = document.querySelector("header");
+	if (window.scrollY > 800) {
+		header.classList.add("scroll");
+	} else {
+		header.classList.remove("scroll");
+	}
+});
 
-  imagesLoaded(grid, () => {
-    new Masonry(grid, {
-      itemSelector: ".grid-item",
-      columnWidth: ".grid-sizer",  // ✅ %폭 기준
-      gutter: 16,                  // ✅ 간격
-      percentPosition: true,        // ✅ %계산
-      fitWidth: false               // ✅ 100% 꽉 채우기
-    });
-  });
+// graphic design masonry layout
+document.addEventListener("DOMContentLoaded", () => {
+	const grid = document.querySelector(".grid");
+	if (!grid) return;
+
+	imagesLoaded(grid, () => {
+		new Masonry(grid, {
+			itemSelector: ".grid-item",
+			columnWidth: ".grid-sizer",  // ✅ %폭 기준
+			gutter: 16,                  // ✅ 간격
+			percentPosition: true,        // ✅ %계산
+			fitWidth: false               // ✅ 100% 꽉 채우기
+		});
+	});
 });
